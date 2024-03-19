4045f5803dda65eb3d1eadc1360aacfedf44ec18
c4ei <his001_ccp@naver.com>
2024-03-19 11:08:35 +0900

    copy

--- src/components/Copyright/index.tsx
+++ src/components/Copyright/index.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Copyright() {
   return (
     <>
-      Powered by{' '}
+      {/* Powered by{' '}
       <a href="https://onout.org/dex" target="_blank" rel="noopener noreferrer">
         OnOut - no-code tool for creating DEX
-      </a>
+      </a> */}
     </>
   )
 }
